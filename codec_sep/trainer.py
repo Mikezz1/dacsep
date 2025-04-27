move masker after rvq to fix gradient flow

codec_sep/trainer.py
@@ -34,110 +34,35 @@ def eval_epoch(
     si_sdr_pseudo_lowpass = 0.0
 
     for batch in tqdm(loader):
-        noisy_audio = batch[0].to(device)
-        clean_audio = batch[1].to(device)
-        ref_audio = batch[2].to(device)
+        noisy_audio = batch[0].to(device).unsqueeze(1)
+        clean_audio = batch[1].to(device).unsqueeze(1)
+        ref_audio = batch[2].to(device).unsqueeze(1)
 
         with torch.no_grad():
             with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-                input_codes = model.quantizer.encode(
-                    noisy_audio.unsqueeze(1), num_quantizers=8
-                )["audio_codes"]
-                target_codes = model.quantizer.encode(
-                    clean_audio.unsqueeze(1), num_quantizers=8
-                )["audio_codes"]
+                out = model(
+                    mix=noisy_audio,
+                    speaker=clean_audio,
+                )
+                codes = model.quantizer.quantizer.encode(out, num_quantizers=8).transpose(0,1)
+                out_waveform = model.quantizer.decode(codes).audio_values.squeeze()
+                codes = model.quantizer.encode(clean_audio, num_quantizers=8).audio_codes
+                reconstructed_target = model.quantizer.decode(codes).audio_values.squeeze()
 
-                speaker_codes = model.quantizer.encode(
-                    ref_audio.unsqueeze(1), num_quantizers=8
-                )["audio_codes"]
-
-                for j in range(model.num_codebooks):
-                    x_j = input_codes
-                    if j > 0:
-                        past_tokens_list = target_codes[
-                            :, :j, :
-                        ]  # [target_codes[:, k, :] for k in range(j)]
-                    else:
-                        past_tokens_list = None
-
-                    logits_j = model(
-                        current_tokens=x_j,
-                        codebook_idx=j,
-                        speaker_emb=None,
-                        past_codebook_tokens=past_tokens_list,
-                        speaker_codes=speaker_codes,
-                    )  # [B, T, 2048]9
-
-                    y_j = target_codes[:, j, :]  # [B, T]
-
-                    loss_j = criterion(logits_j.reshape(-1, 2048), y_j.reshape(-1))
-                    total_loss += loss_j.item()
-
-                predicted_codes = [None] * model.num_codebooks
-                for j in range(model.num_codebooks):
-                    # We'll use the PREDICTED codes from [0..j-1], if j>0
-                    past_tokens_list = []
-                    for past_idx in range(j):
-                        # shape => [B, T_code]
-                        past_tokens_list.append(predicted_codes[past_idx])
-
-                    if len(past_tokens_list) > 0:
-                        past_tokens_list = torch.stack(past_tokens_list, dim=1)
-                    else:
-                        past_tokens_list = None
-                    x_j_input = input_codes
-
-                    with torch.no_grad():
-                        logits_j = model(
-                            current_tokens=x_j_input,  # shape [B, T]
-                            codebook_idx=j,
-                            speaker_emb=None,
-                            past_codebook_tokens=past_tokens_list,
-                            speaker_codes=speaker_codes,
-                        )
-                        pred_j = torch.argmax(logits_j, dim=-1)
-
-                    predicted_codes[j] = pred_j
-                predicted_codes_stacked = torch.stack(predicted_codes, dim=1)
-                with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-                    with torch.no_grad():
-                        pred_waveform = model.quantizer.decode(
-                            predicted_codes_stacked
-                        ).audio_values.squeeze()
-                        pred_gt = model.quantizer.decode(
-                            target_codes
-                        ).audio_values.squeeze()
                 si_sdr += (
-                    scale_invariant_signal_distortion_ratio(pred_waveform, clean_audio)
+                    scale_invariant_signal_distortion_ratio(out_waveform, clean_audio.squeeze())
                     .mean()
                     .detach()
                     .cpu()
                 )
                 si_sdr_pseudo += (
-                    scale_invariant_signal_distortion_ratio(pred_waveform, pred_gt)
+                    scale_invariant_signal_distortion_ratio(out_waveform, reconstructed_target)
                     .mean()
                     .detach()
                     .cpu()
                 )
-                # si_sdr += scale_invariant_signal_distortion_ratio(
-                #     torchaudio.functional.resample(pred_waveform,24000,16000),
-                #     torchaudio.functional.resample(clean_audio,24000,16000)
-                #     ).mean().detach().cpu()
-                # si_sdr_pseudo += scale_invariant_signal_distortion_ratio(
-                #     torchaudio.functional.resample(pred_waveform, 24000, 16000),
-                #     torchaudio.functional.resample(pred_gt, 24000, 16000),
-                #     ).mean().detach().cpu()
-                # si_sdr_pseudo_lowpass += scale_invariant_signal_distortion_ratio(
-                #     torchaudio.functional.lowpass_biquad(pred_waveform.float(), sample_rate=24000, cutoff_freq=8000),
-                #     torchaudio.functional.lowpass_biquad(pred_gt.float(), sample_rate=24000, cutoff_freq=8000)
-                #     ).mean().detach().cpu()
-
-                # si_sdr_mix += scale_invariant_signal_distortion_ratio(noisy_audio, clean_audio).mean().detach().cpu()
-                # pesqs += pesq(torchaudio.functional.resample(noisy_audio,24000,16000).float(),
-                #               torchaudio.functional.resample(clean_audio,24000,16000).float(),fs=16000, mode='wb').detach().cpu()
 
     return (
-        total_loss / len(loader),
         (si_sdr / len(loader)).item(),
         (si_sdr_pseudo / len(loader)).item(),
     )  # , (si_sdr_pseudo_lowpass  / len(loader)).item()
@@ -145,7 +70,7 @@ def eval_epoch(
 
 USE_SPEAKER_EMBEDDER = False
 N_CODEBOOKS = 8
-
+O = False
 
 def train_epoch(
     loader,
@@ -159,10 +84,13 @@ def train_epoch(
     cfg,
 ):
     total_loss = 0
+    global O
+
     for batch in tqdm(loader):
-        noisy_audio = batch[0].to(device)
-        clean_audio = batch[1].to(device)
-        ref_audio = batch[2].to(device)
+        optimizer.zero_grad()
+        noisy_audio = batch[0].to(device).unsqueeze(1)
+        clean_audio = batch[1].to(device).unsqueeze(1)
+        ref_audio = batch[2].to(device).unsqueeze(1)
 
         if cfg.variable_len_train:
             max_len_sample = np.random.randint(1, 7) * 24000
@@ -174,82 +102,44 @@ def train_epoch(
             ref_audio = ref_audio[:, :max_len_ref]
 
         with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-            input_codes = model.quantizer.encode(
-                noisy_audio.unsqueeze(1), num_quantizers=8
-            )["audio_codes"]
-            target_codes = model.quantizer.encode(
-                clean_audio.unsqueeze(1), num_quantizers=8
-            )["audio_codes"]
+            out = model(
+                mix=noisy_audio,
+                speaker=ref_audio,
+            )
+            codes = model.quantizer.quantizer.encode(out, num_quantizers=8).transpose(0,1)
+            out_waveform = model.quantizer.decode(codes).audio_values.squeeze()
+            codes = model.quantizer.encode(clean_audio, num_quantizers=8).audio_codes
+            reconstructed_target = model.quantizer.decode(codes).audio_values.squeeze()
 
-            if cfg.add_si_sdr_loss:
-                reconstructed_target = model.quantizer.decode(
-                    target_codes
-                ).audio_values.squeeze()
+            if not O:
+                # print(out_waveform.float().cpu().detach().shape)
+                torchaudio.save('sample.wav', out_waveform[0].float().cpu().detach().unsqueeze(0), sample_rate=24000)
+                torchaudio.save('sample_rec.wav', reconstructed_target[0].float().cpu().detach().unsqueeze(0), sample_rate=24000)
+                torchaudio.save('sample_tgt.wav', clean_audio[0].cpu().float(), sample_rate=24000)
+                O = True
 
-            speaker_codes = None
-            speaker_codes = model.quantizer.encode(
-                ref_audio.unsqueeze(1), num_quantizers=8
-            )["audio_codes"]
-            speaker_emb = None
+            si_sdr_loss = -scale_invariant_signal_distortion_ratio(
+                out_waveform, reconstructed_target.squeeze()
+            )
 
-        js = list(range(N_CODEBOOKS))
-        # np.random.shuffle(js)
-        optimizer.zero_grad()
-        _preds_final = []
-        for j in js:
-            x_j = input_codes
-            if j > 0:
-                past_tokens_list = target_codes[
-                    :, :j, :
-                ]  # [target_codes[:, k, :] for k in range(j)]
-            else:
-                past_tokens_list = None
+            # print(si_sdr_loss[0])
 
-            with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-                logits_j = model(
-                    current_tokens=x_j,
-                    codebook_idx=j,
-                    speaker_emb=speaker_emb,
-                    past_codebook_tokens=past_tokens_list,
-                    speaker_codes=speaker_codes,
-                )  # [B, T, 2048]9
+            si_sdr_loss = si_sdr_loss.mean()
 
-                y_j = target_codes[:, j, :]  # [B, T]
+            writer.add_scalar(
+                f"Loss/train", si_sdr_loss.detach().cpu(), step
+            )
+            writer.add_scalar(f"LR/train", scheduler.get_lr()[-1], step)
 
-                if cfg.add_si_sdr_loss:
-
-                    _preds_final.append(torch.argmax(logits_j, dim=-1))
-
-                    if (j == 7) or cfg.add_si_sdr_loss_every_codebook:
-                        preds = torch.stack(_preds_final, dim=1)
-                        reconstructed_pred = model.quantizer.decode(
-                            preds
-                        ).audio_values.squeeze()
-
-                        si_sdr_loss = -scale_invariant_signal_distortion_ratio(
-                            reconstructed_pred, reconstructed_target
-                        ).mean()
-                    else:
-                        si_sdr_loss = 0.0
-
-                loss_j = criterion(logits_j.reshape(-1, 2048), y_j.reshape(-1))
-                writer.add_scalar(
-                    f"Loss/train/codebook_{j}", loss_j.detach().cpu(), step
-                )
-                writer.add_scalar(f"LR/train", scheduler.get_lr()[-1], step)
-
-                if cfg.add_si_sdr_loss:
-                    loss_j += si_sdr_loss
-                    if (j == 7) or  cfg.add_si_sdr_loss_every_codebook:
-                        writer.add_scalar(
-                            f"Loss/train/si_sdr_loss_{j}", si_sdr_loss.detach().cpu(), step
-                        )
-
-                total_loss += loss_j.item()
-                loss_j *= 1 / (j + 1) ** 0.5
-                loss_j.backward()
-        optimizer.step()
-        scheduler.step()
+            total_loss += si_sdr_loss
+            si_sdr_loss.backward()
+            # for name, p in model.quantizer.quantizer.named_parameters():
+            #     if p.requires_grad:
+            #         # print(name, p.shape, p.grad)
+            #         print(f"{name}: grad = {p.grad:.4g}")
+            # break
+            optimizer.step()
+            scheduler.step()
         step += 1
     return total_loss, step
 
@@ -275,6 +165,8 @@ def train(
     writer = SummaryWriter(log_dir=log_dir)
     scheduler.step()
     step = 0
+    if cfg.overfit:
+        loader = [next(iter(loader))]
     for epoch in range(0, num_epochs):
         model.train()
 
@@ -292,25 +184,24 @@ def train(
             cfg=cfg,
         )
 
-        writer.add_scalar(f"Loss/train_epoch", total_loss / len(loader), epoch)
+        writer.add_scalar(f"Loss/train_epoch", total_loss / len(loader) if not cfg.overfit else total_loss, epoch)
         print(
             f"Epoch {epoch}: total_loss={total_loss / len(loader) :.3f}, lr: {scheduler.get_lr()[-1]}"
         )  # len(loader) #/ len(loader)
-        if epoch % eval_every == 0:
-            val_loss, val_sdr, val_sdr_pseudo = eval_epoch(
+        if (epoch % eval_every == 0) and not cfg.overfit:
+            val_sdr, val_sdr_pseudo = eval_epoch(
                 val_loader, model, device=device, criterion=criterion
             )
-            writer.add_scalar(f"Loss/val_epoch", val_loss, epoch)
             writer.add_scalar(f"SDR/val_epoch", val_sdr, epoch)
             writer.add_scalar(f"SDR_PSEUDO/val_epoch", val_sdr_pseudo, epoch)
-        if epoch % 50 == 0:
+        if (epoch % 20 == 0) and not cfg.overfit:
             torch.save(
                 model,
                 os.path.join(
                     c_root, f"model_{epoch}_ep_loss_{total_loss / len(loader)}.pt"
                 ),
             )
-            # torch.save(optimizer, os.path.join(c_root, f'optimizer_{epoch}_ep_loss_{total_loss / len(loader)}.pt'))
-            # torch.save(scheduler, os.path.join(c_root, f'scheduler_{epoch}_ep_loss_{total_loss / len(loader)}.pt'))
+            torch.save(optimizer, os.path.join(c_root, f'optimizer_{epoch}_ep_loss_{total_loss / len(loader)}.pt'))
+            torch.save(scheduler, os.path.join(c_root, f'scheduler_{epoch}_ep_loss_{total_loss / len(loader)}.pt'))
     writer.flush()
     print("Training complete!")

train.py
@@ -1,6 +1,6 @@
 from codec_sep.trainer import *
 from codec_sep.data import LibrimixDataset
-from codec_sep.model import TransformerEncoderWithCodebookCondition
+from codec_sep.model import TransformerEncoderWithCodebookCondition, TransformerEncoderLatent
 from codec_sep.utils import _to_namespace, count_parameters, lr_lambda
 
 
@@ -29,7 +29,7 @@ from functools import partial
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda")
+    device = torch.device("cuda:0")
 
     torch.backends.cudnn.benchmark = False
     # torch.set_float32_matmul_precision("medium")
@@ -42,29 +42,44 @@ if __name__ == "__main__":
     print(cfg)
 
     mimi_model = MimiModel.from_pretrained(
-        "kyutai/mimi", cache_dir="/mike_migrate2/hf_models"
+        "kyutai/mimi", cache_dir="/mike_migrate2/hf_models2"
     )
     mimi_model = mimi_model.to(device)
 
-    model = TransformerEncoderWithCodebookCondition(
+    model = TransformerEncoderLatent(
         quantizer=mimi_model,
         vocab_size=2048,
         embed_dim=256,
         num_heads=16,
         num_layers=16,
         hidden_dim=1024,
-        num_codebooks=cfg.num_codebooks,
-        use_speaker_embed=USE_SPEAKER_EMBEDDER,
     ).to(device)
 
     print('Model size: ', count_parameters(model) / 1e6)
 
     criterion = nn.CrossEntropyLoss()
+    trainable = (
+        p for n, p in model.named_parameters()
+        if p.requires_grad and not n.startswith("quantizer")
+    )
+
+    # for n, p in model.named_parameters():
+    #     if p.requires_grad and  n.startswith("quantizer"):
+    #         print(n)
+
     optimizer = torch.optim.AdamW(
-        (p for p in model.parameters() if p.requires_grad),
+        trainable,
         lr=cfg.lr,
         weight_decay=cfg.weight_decay,
     )  # 2.2e-3
+
+    # for i, g in enumerate(optimizer.param_groups):
+    #     print(f"group {i}")
+    #     for p in g["params"]:
+    #         print("   ", p.shape, id(p))
+
+    # for name, p in model.named_parameters():
+    #     print(name, id(p), p.requires_grad)
 
     dataset = LibrimixDataset(
         data_dir="/mike_migrate2/data_16khz/Libri2Mix/wav16k/min/train-100/",
@@ -80,7 +95,7 @@ if __name__ == "__main__":
 
     # If each example in the dataset has the same length, the default collate will stack them
     loader = DataLoader(
-        dataset, batch_size=cfg.train.batch_size, shuffle=True, num_workers=6
+        dataset, batch_size=cfg.train.batch_size, shuffle=True if not cfg.overfit else False, num_workers=6
     )
     val_loader = DataLoader(
         dataset_val, batch_size=cfg.val.batch_size, shuffle=False, num_workers=2